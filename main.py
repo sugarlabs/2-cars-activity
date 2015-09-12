892dcf0613f3ea54a251a883bd3715d7f586ca32
iamutkarshtiwari <iamutkarshtiwari@gmail.com>
2015-09-12 07:32:21 +0530

    car movement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Flappy Birds
+# 2 Cars
 # Copyright (C) 2015  Utkarsh Tiwari
 #
 # This program is free software: you can redistribute it and/or modify
@@ -26,10 +26,6 @@ import gtk
 import pygame
 import sys
 
-from elements import bird
-from elements import pillar
-from elements import load_elements_images
-from welcomescreen import welcomescreen
 
 
 class game:
@@ -40,18 +36,49 @@ class game:
         
     def initialize(self):
 
-        self.flag=0
-        self.scores=0
         
-        self.land1x=350
-        self.land2x=840
         
-        self.keyinit=0
-        self.keytest=0
         
-        self.flag=0
-        self.welcomeflag=1
-        self.musicflag=False
+        self.left=True
+        self.right=True
+        
+        self.leftclick=self.rightclick=0
+        
+        self.leftmove=0
+        self.rightmove=0
+        
+        
+        
+        
+        self.leftcar_x=390
+        #510
+        
+        
+        self.rightcar_x=760
+        #600
+        
+        self.speed=8
+        
+        
+        
+        
+        
+        #self.welcomeflag=1
+        #self.musicflag=False
+        
+        #self.left_transit=self.right_transit=0
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         pygame.init()
         self.sound=True
@@ -74,12 +101,9 @@ class game:
             #gameicon=pygame.image.load('images/icon.png')
             #pygame.display.set_icon(gameicon)
 
-        self.hit=pygame.mixer.Sound("assets/sounds/hit.ogg")
+        #self.hit=pygame.mixer.Sound("assets/sounds/hit.ogg")
         
-        self.point=pygame.mixer.Sound("assets/sounds/point.ogg")
-        self.wing=pygame.mixer.Sound("assets/sounds/wing.ogg")
-        self.swoosh=pygame.mixer.Sound("assets/sounds/swoosh.ogg")
-        self.die=pygame.mixer.Sound("assets/sounds/die.ogg")
+        
         
         self.font_path = "fonts/sans.ttf"
         self.font_size = 55
@@ -87,16 +111,26 @@ class game:
         self.font2=pygame.font.Font("fonts/sans.ttf",30)
         self.font3=pygame.font.Font("fonts/sans.ttf",40)
         self.font4=pygame.font.Font("fonts/sans.ttf",23)
+        
+        
+        
+        
 
         # Load the images for elements
-        load_elements_images()
-
-        self.birds=bird()
-
-        self.pillarlist=[]
+        #load_elements_images()
         
-        a=pillar()
-        self.pillarlist.append(a)
+        self.background = pygame.transform.scale(pygame.image.load("assets/background.png").convert(),\
+                                                 (491,768))
+        
+        self.leftcar = pygame.transform.scale(pygame.image.load("assets/bluecar.png"),\
+                                              (45,90))
+        
+        
+        self.rightcar = pygame.transform.scale(pygame.image.load("assets/redcar.png"),\
+                                              (45,90))
+        
+        
+      
         
 
     def make(self):
@@ -110,27 +144,7 @@ class game:
             
         crashed=False   
 
-        # Sound loads
         
-        self.hit=pygame.mixer.Sound("assets/sounds/hit.ogg")
-        
-        self.point=pygame.mixer.Sound("assets/sounds/point.ogg")
-        #wing=pygame.mixer.Sound("assets/sounds/wing.ogg")
-        #swoosh=pygame.mixer.Sound("assets/sounds/swoosh.ogg")
-        #die=pygame.mixer.Sound("assets/sounds/die.ogg")
-
-        #image loads
-
-        land=pygame.image.load("assets/land.png").convert()
-        land1=pygame.transform.scale(land,(490,150))
-        land2=land1
-        
-        sky=pygame.image.load("assets/sky.png").convert()
-        sky=pygame.transform.scale(sky,(490,200))
-            
-        
-        skyfill=pygame.image.load("assets/skyfill.png").convert()
-        skyfill=pygame.transform.scale(skyfill,(490,500))
         
         # GAME LOOP BEGINS !!!
         
@@ -146,77 +160,139 @@ class game:
                 
             mos_x,mos_y=pygame.mouse.get_pos() 
             
+            
+            '''
             if self.welcomeflag == 1:
                 a = welcomescreen(self.gameDisplay)
                 a.run()
                 self.welcomeflag=0
                 self.keyinit=1
                 #self.dispinit()
-                
-            self.gameDisplay.fill(white)
+            '''    
             
-            self.gameDisplay.blit(skyfill,(350,0))
-            self.gameDisplay.blit(sky,(350,400))
+            
+            self.gameDisplay.fill(white)
+            self.gameDisplay.blit(self.background,(350,0))
+            
+            
+            
+            # Car Blitting
+            
+            self.gameDisplay.blit(self.leftcar,(self.leftcar_x,550))
+            
+            self.gameDisplay.blit(self.rightcar,(self.rightcar_x,550))
+            
+            
+            #self.gameDisplay.blit(self.leftcar,(510,550))
+            
+            #self.gameDisplay.blit(self.rightcar,(600,550))
+            
+            
+            
+            #390
+            #510
+        
+        
+            #760
+            #600
+            
+            
+            if(self.leftmove==1):
+                
+                
+                #For left car updation
+                if(self.left==True):
+                    
+                    self.leftcar_x+=self.speed
+                    
+                    if(self.leftcar_x>=510):
+                        
+                        self.leftmove=0
+                        self.left=not self.left
+                        
+                else:
+                    self.leftcar_x-=self.speed
+                    
+                    if(self.leftcar_x<=390):
+                        
+                        self.leftmove=0
+                        self.left=not self.left
+                        
+                        
+            if(self.rightmove==1):            
+                
+                #For right car updation
+                if(self.right==True):
+                    
+                    self.rightcar_x-=self.speed
+                    
+                    if(self.rightcar_x<=640):
+                        
+                        self.rightmove=0
+                        self.right= not self.right
+                        
+                    
+                else:
+                    
+                    self.rightcar_x+=self.speed
+                    
+                    if(self.rightcar_x>=760):
+                        
+                        self.rightmove=0
+                        self.right=not self.right
+                        
+                        
+                        
+                        
+                
+                
+                
+            
+            
+            
+            
+            # Keyboard Input
+            
+            #event = pygame.event.poll()
+            
+            if event.type==pygame.KEYDOWN and event.key==276 and self.leftclick==0:
+                #jump.play(0)
+                #self.left_transit=1
+                self.leftmove=1
+                self.leftclick=1
+                
+                #self.left=not self.left
+            
+            #left starts moving
+            
+            
+            
+            if event.type==pygame.KEYUP and event.key==276:
+                self.leftclick=0
+                
+                
+                    
+            #event = pygame.event.poll()    
+            
+            if event.type==pygame.KEYDOWN and event.key==275 and self.rightclick==0:
+                #jump.play(0)
+                self.rightmove=1
+                self.rightclick=1
+                
+                
+            #right start moving
+            
+            
+            
+            if event.type==pygame.KEYUP and event.key==275:
+                self.rightclick=0
+            
+            
+            
+            
+            
             
           
-            # Pillar Display
-            for i in self.pillarlist:
-                i.display(self.gameDisplay,self.pillarlist,self.birds,self)
-                
-                
-                if(self.welcomeflag==1):
-                    a=welcomescreen(self.gameDisplay)
-                    a.run()
-                    self.welcomeflag=0
-                    self.keyinit=1
-                    break
-
-            # Platform blit
-            self.gameDisplay.blit(land1,(self.land1x,600))
-            self.gameDisplay.blit(land2,(self.land2x,600))
-            
-            self.land1x-=3
-            self.land2x-=3
-            
-            if(self.land1x<=-140):
-                self.land1x=837
-                
-            if(self.land2x<=-140):
-                self.land2x=837
-
-            if(self.keyinit==1):
-                self.birds.jump(land1,land2,self.land1x,self.land2x,self)
-            
-            if(self.welcomeflag==1):
-                a=welcomescreen(self.gameDisplay)
-                a.run()
-                self.welcomeflag=0
-                self.keyinit=1
-                continue
-            
-            if event.type==pygame.KEYDOWN and event.key==273 and self.keytest==0:
-                self.keyinit=self.keytest=1
-                if(self.birds.t>25):
-                    self.birds.angle=0
-                
-                self.birds.t=0
-                            
-            if event.type==pygame.KEYUP  and event.key==273 and self.musicflag:
-                
-                self.keytest=0
-                self.musicflag=False
-            
-            if not(self.musicflag) and event.type==pygame.KEYDOWN  and event.key==273: 
-                pygame.mixer.music.load("assets/sounds/wing.ogg")
-                pygame.mixer.music.play(0,0)
-                self.musicflag= True
-
-            # bird display
-            
-            self.birds.display(self.gameDisplay,self.flag)
-            
-            head3=self.font3.render(str(self.scores),1,(white))
-            self.gameDisplay.blit(head3,(580,30))
             
             # BLACK RECTANGLES DISPLAY
                       
@@ -226,6 +302,11 @@ class game:
             pygame.draw.rect(self.gameDisplay,black,(0,0,350,768))    
                     
             pygame.draw.rect(self.gameDisplay,black,(840,0,693,768))
+            
+            
+            
+            
+            
             
             pygame.display.update()
             clock.tick(60)
